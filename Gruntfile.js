841651c4d84a30a2b253682ea6581a1d907fb7ce
Korbinian Polk <korbinianpolk@googlemail.com>
2015-05-13 16:35:19 +0200

    grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-sync');
+	grunt.initConfig({
+		concat: {
+			dist: {
+				src: 'dev/scripts/*.js',
+				dest: 'build/scripts/script.js'
+			}
+		},
+		uglify: {
+			dist: {
+				options: {
+					banner: '// <%= grunt.template.today("dd.mm.yyyy") %> by Korbinian Polk\n',
+					sourceMap: true 
+				},
+				src: '<%= concat.dist.dest %>',
+				dest: 'build/scripts/script.min.js'
+			}
+		},
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'build/styles/style.css': 'dev/styles/style.scss',
+
+				}
+			}
+		},
+		autoprefixer: {
+			dist: {
+				src: 'build/styles/style.css'
+			}
+		},
+		sync: {
+			dist: {
+				files: [{
+					cwd: 'dev',
+					src: [
+						'*.html'
+					],
+					dest: 'build',
+
+				}]
+			}
+		},
+		watch: {
+			styles: {
+				files: 'dev/styles/*.scss',
+				tasks: ['sass', 'autoprefixer']
+			},
+			scripts: {
+				files: ['dev/scripts/*.js'],
+				tasks: ['concat', 'uglify']
+			},
+			sync: {
+				files: ['dev/*.html'],
+				tasks: ['sync']
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['sass', 'autoprefixer']);
+};
