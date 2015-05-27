74ab3b433067035e9599ac6c161dfdd9cea9094c
Korbinian Polk <korbinianpolk@googlemail.com>
2015-05-27 18:14:52 +0200

    added page-transition animations for ng-leave (hurray!)

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,102 +1,101 @@
 var sba = angular.module('sba', [
+
+    // add dependencies
+
         'ui.router', 
         'ngAnimate', 
         'ngTouch', 
         'ui.bootstrap'
+
     ])
+
     .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
-    $urlRouterProvider.otherwise('/');
+        // route to root if url does not exist
 
-    $stateProvider
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+
+            .state('home', {
+                url: '/',
+                templateUrl: 'templates/pages/home.html',
+                controller: 'homeController',
+                bla: 'home'
+            })
+
+            .state('news', {
+                url: '/news',
+                templateUrl: 'templates/pages/news.html',
+                controller: 'newsController',
+                bla: 'news'
+            })
 
 
-    .state('home', {
-        url: '/',
-        templateUrl: 'templates/pages/home.html',
-        controller: 'homeController',
+            .state('projects', {
+                url: '/projects',
+                templateUrl: 'templates/pages/projects.html',
+                controller: 'projectsController',
+                bla: 'projects'
+            })
+
+            .state('project022', {
+                url: '/projects/022',
+                templateUrl: 'templates/projects/022.html',
+                controller: 'projectController',
+            })
+            .state('project055', {
+                url: '/projects/055',
+                templateUrl: 'templates/projects/055.html',
+                controller: 'projectController',
+            })
+
+            .state('profile', {
+                url: '/profile',
+                templateUrl: 'templates/pages/profile.html',
+                controller: 'profileController',
+                bla: 'profile'
+            })
+
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'templates/pages/contact.html',
+                controller: 'contactController',
+                bla: 'contact'
+            })
+    }])
+    
+    .controller('homeController', function($scope) {
+        $scope.pageClass = 'page-home';
+    })
+    .controller('newsController', function($scope) {
+        $scope.pageClass = 'page-news';
+    })
+    .controller('projectsController', function($scope) {
+        $scope.pageClass = 'page-projects';
+    })
+    .controller('projectController', function($scope) {
+        $scope.pageClass = 'page-project';
+    })
+    .controller('profileController', function($scope) {
+        $scope.pageClass = 'page-profile';
+    })
+    .controller('contactController', function($scope) {
+        $scope.pageClass = 'page-contact';
     })
 
+// uses angular ui
 
-    .state('news', {
-        url: '/news',
-        templateUrl: 'templates/pages/news.html',
-        controller: 'newsController',
-    })
-
-
-    .state('projects', {
-        url: '/projects',
-        templateUrl: 'templates/pages/projects.html',
-        controller: 'projectsController',
-    })
-
-    .state('project022', {
-        url: '/projects/022',
-        templateUrl: 'templates/projects/022.html',
-        controller: 'projectController',
-    })
-    .state('project055', {
-        url: '/projects/055',
-        templateUrl: 'templates/projects/055.html',
-        controller: 'projectController',
-    })
-
-    .state('profile', {
-        url: '/profile',
-        templateUrl: 'templates/pages/profile.html',
-        controller: 'profileController',
-    })
-
-
-    .state('contact', {
-        url: '/contact',
-        templateUrl: 'templates/pages/contact.html',
-        controller: 'contactController',
-    });
-}]);
-
-sba.controller('homeController', function($scope) {
-    $scope.pageClass = 'page-home';
-
-});
-
-sba.controller('newsController', function($scope) {
-    $scope.pageClass = 'page-news';
-
-});
-
-sba.controller('projectsController', function($scope) {
-    $scope.pageClass = 'page-projects';
-
-});
-sba.controller('projectController', function($scope) {
-    $scope.pageClass = 'page-project';
-
-});
-
-sba.controller('profileController', function($scope) {
-    $scope.pageClass = 'page-profile';
-
-});
-
-sba.controller('contactController', function($scope) {
-    $scope.pageClass = 'page-contact';
-
-});
-
-// uses angular
-
-sba.controller('CarouselDemoCtrl', function ($scope) {
+.controller('CarouselDemoCtrl', function ($scope) {
     $scope.myInterval = 1000;
     var slides = $scope.slides = [
         { image: 'images/slider/slider-1.jpg' },
         { image: 'images/slider/slider-2.jpg' },
         { image: 'images/slider/slider-3.jpg' }
     ];
-});
+})
 
-sba.controller('AccordionDemoCtrl', function ($scope) {
+.controller('AccordionDemoCtrl', function ($scope) {
   $scope.oneAtATime = true;
 
   $scope.groups = [
@@ -121,13 +120,15 @@ sba.controller('AccordionDemoCtrl', function ($scope) {
     isFirstOpen: true,
     isFirstDisabled: false
   };
-});
+})
 
-sba.controller('viewController', function ($scope){
+.controller('viewController', function ($scope, $state){
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-        console.log(fromState.name);
-        console.log(toState.name);
         $scope.fromState = fromState;
         $scope.toState = toState;
-    })
+        $scope.bla = $state.current.bla;
+        console.log($scope.bla);
+    });
+
+    
 });
